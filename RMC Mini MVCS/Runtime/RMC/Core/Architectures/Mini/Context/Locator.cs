9b75740e89494a12c7b97f87b42905065f5c0b35
Samuel Asher Rivello <63511769+SamuelAsherRivello@users.noreply.github.com>
2023-10-09 21:40:09 -0300

    Add Tests For Locator

--- RMC Mini MVCS/Runtime/RMC/Core/Architectures/Mini/Context/Locator.cs
+++ RMC Mini MVCS/Runtime/RMC/Core/Architectures/Mini/Context/Locator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace RMC.Core.Architectures.Mini.Context
@@ -28,24 +29,51 @@ namespace RMC.Core.Architectures.Mini.Context
 		//  Methods ---------------------------------------
 		public void AddItem (IItem item)
 		{
-			if (HasItem<IItem>())
+			//Strict means *ONLY* allow 1 instance of T
+			//But *DO* allow 2+ subclasses of T 
+			//And *DO* allow 2+ sibling subclasses of T
+			if (HasItem<IItem>(true) 
+			    && GetItem<IItem>(true).GetType() == item.GetType())
 			{
 				// Allow MAX 0 or 1 instance of T
 				throw new Exception("AddItem() failed. Item already added. Call HasItem<T>() first.");
 			}
+
 			_items.Add(item);
 			OnAddItemCompleted.Invoke(item);
 		}
 		
-		public bool HasItem<SubType>() where SubType :IItem 
+		public bool HasItem<SubType>(bool isStrict = false) where SubType :IItem 
 		{
-			return GetItem<SubType>() != null;
+			return GetItem<SubType>(isStrict) != null;
 		}
 		
-		public SubType GetItem<SubType>() where SubType :IItem 
+		
+		public SubType GetItem<SubType>(bool isStrict = false) where SubType :IItem
 		{
-			return _items.OfType<SubType>().ToList().FirstOrDefault<SubType>();
+			SubType instance;
+
+			if (isStrict)
+			{
+				instance = _items.OfType<SubType>().ToList().FirstOrDefault<SubType>();
+				if (typeof(SubType).IsInterface)
+				{
+					//Here we do NOT match if they are NOT the same type
+					//regardless if they share an interface
+					if (!(instance?.GetType() is SubType))
+					{
+						instance = default(SubType);
+					}
+				}
+			}
+			else
+			{
+				instance = _items.OfType<SubType>().ToList().FirstOrDefault<SubType>();
+			}
+		
+			return instance;
 		}
+		
 		
 		public IItem GetItem(Type type)
 		{
@@ -54,11 +82,46 @@ namespace RMC.Core.Architectures.Mini.Context
 
 		public void RemoveItem(IItem item)
 		{
-			if (!HasItem<IItem>())
+			//Strict means *ONLY* allow 1 instance of T
+			//But *DO* allow 2+ subclasses of T 
+			//And *DO* allow 2+ sibling subclasses of T
+			if (HasItem<IItem>(true) 
+			    && GetItem<IItem>(true).GetType() == item.GetType())
 			{
+				// Allow MAX 0 or 1 instance of T
 				throw new Exception("RemoveItem() failed. Must call HasItem<T>() first.");
 			}
+			
 			_items.Remove(item);
+		}
+		
+		public void RemoveItem<SubType>(IItem item) where SubType : IItem 
+		{
+			//Strict means *ONLY* allow 1 instance of T
+			//But *DO* allow 2+ subclasses of T 
+			//And *DO* allow 2+ sibling subclasses of T
+			if (!(HasItem<SubType>(true) 
+			    && GetItem<SubType>(true).GetType() == item.GetType()))
+			{
+				// Allow MAX 0 or 1 instance of T
+				throw new Exception("RemoveItem() failed. Must call HasItem<T>() first.");
+			}
+			
+			_items.Remove(item);
+		}
+		
+		public void RemoveItem<SubType>() where SubType : IItem 
+		{
+			//Strict means *ONLY* allow 1 instance of T
+			//But *DO* allow 2+ subclasses of T 
+			//And *DO* allow 2+ sibling subclasses of T
+			if (!HasItem<SubType>(true))
+			{
+				// Allow MAX 0 or 1 instance of T
+				throw new Exception("RemoveItem() failed. Must call HasItem<T>() first.");
+			}
+			
+			_items.Remove(GetItem<SubType>(true));
 		}
 		
 		//  Event Handlers --------------------------------
