b9e57e9f0bb783b12393a69cf201b9c27779ef7b
Samuel Asher Rivello <63511769+SamuelAsherRivello@users.noreply.github.com>
2023-10-22 12:28:30 -0300

    Revert "Revert "Add Modules. WIP""
    This reverts commit 473e65066e5c7f897088a69c265c88f76ac24543.

---
+++ RMC Mini MVCS/Runtime/RMC/Core/Experimental/Architectures/Mini/Module/IModule.cs
@@ -0,0 +1,15 @@
+
+using RMC.Core.Architectures.Mini.Context;
+
+namespace RMC.Core.Experimental.Architectures.Mini.Module
+{
+    /// <summary>
+    /// The View handles user interface and user input
+    /// </summary>
+    public interface IModule : IMiniMvcsWithModules
+    {
+        //  Properties ------------------------------------
+
+        //  Methods ---------------------------------------
+    }
+}
